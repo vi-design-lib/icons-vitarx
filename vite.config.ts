aed0cecf5a3a0014170e1932021dc457f457cf10
ZhuChongLin <8210856@qq.com>
2025-01-05 21:46:32 +0800

    build(vite): 更新构建配置并移除 dts 插件
    - 移除了 vite-plugin-dts 插件
    - 修改了 build 配置，只生成 umd 格式

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,15 @@
 import { defineConfig } from 'vite'
 import vitarx from 'vite-plugin-vitarx'
-import dtsPlugin from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [
-    vitarx(),
-    dtsPlugin({
-      insertTypesEntry: true,
-      include: ['src'],
-      rollupTypes: true
-    })
-  ],
+  plugins: [vitarx()],
   build: {
     outDir: 'dist',
     lib: {
       entry: 'src/index.ts',
       name: 'Icons',
       fileName: (format) => `icons.${format}.js`,
-      formats: ['es', 'umd']
+      formats: ['umd']
     },
     rollupOptions: {
       external: ['vitarx', 'vitarx/jsx-runtime'],
