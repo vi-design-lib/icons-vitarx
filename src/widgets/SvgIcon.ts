59d2d54becce5e75996db328dc4d8b9f39951972
ZhuChongLin <8210856@qq.com>
2025-01-05 21:13:29 +0800

    refactor(widgets): 重构 SvgIcon 类
    - 移除了 IconProps 类型和相关文档注释
    - 删除了静态属性和方法
    - 简化了类结构，改为抽象类
    - 引入了 Icons 基类
    - 重写了 build 方法

--- src/widgets/SvgIcon.ts
+++ src/widgets/SvgIcon.ts
@@ -1,75 +1,7 @@
-import { createElement, Element, reactive, type WebRuntimeDom, Widget } from 'vitarx'
+import Icons from '@/widgets/Icons.js'
+import { createElement, Element } from 'vitarx'
 
-export type IconProps = {
-  /**
-   * 图标样式类名
-   */
-  className?: WebRuntimeDom.HTMLClassProperties
-  /**
-   * 图标默认颜色
-   */
-  color?: string
-  /**
-   * 图标路径填充颜色
-   *
-   * 可以为具有多个路径的图标，单独设置填充颜色。
-   */
-  pathFill?: string[]
-  /**
-   * 图标尺寸
-   *
-   * @default `1em`
-   */
-  size?: string
-  /**
-   * 图标样式
-   *
-   * @default undefined
-   */
-  style?: WebRuntimeDom.HTMLStyleProperties
-}
-
-/**
- * # Icon抽象类
- *
- * 图标抽象类，用于定义图标的公共属性和构建方法。
- *
- * @abstract
- */
-export default abstract class SvgIcon extends Widget<IconProps> {
-  static #commonProps: IconProps = reactive({
-    size: '1em',
-    color: 'currentColor',
-    style: undefined,
-    className: undefined,
-    pathFill: undefined
-  })
-
-  /**
-   * 设置公共属性
-   *
-   * @param {IconProps} props - 公共属性
-   */
-  static setCommonProps(props: IconProps) {
-    Object.assign(SvgIcon.#commonProps, props)
-  }
-
-  get size() {
-    return this.props.size || SvgIcon.#commonProps.size || '1em'
-  }
-
-  get color() {
-    return this.props.color || SvgIcon.#commonProps.color || 'currentColor'
-  }
-
-  get style() {
-    return this.props.style || SvgIcon.#commonProps.style
-  }
-
-  get className() {
-    return this.props.className || SvgIcon.#commonProps.className
-  }
-
+export default abstract class SvgIcon extends Icons {
   // 子类必须实现，返回svg的viewBox属性
   abstract get viewBox(): string
   // 子类必须实现，返回的数组中的每个元素为path的d属性
@@ -77,18 +9,9 @@ export default abstract class SvgIcon extends Widget<IconProps> {
   // 子类必须实现，返回图标的标签名，用于无障碍访问
   abstract get label(): string
   /**
-   * 获取path的填充颜色
-   *
-   * @private
-   */
-  private get pathFill() {
-    return this.props.pathFill || SvgIcon.#commonProps.pathFill
-  }
-
-  /**
    * @inheritDoc
    */
-  protected build(): Element {
+  protected override build(): Element {
     const children = this.paths.map((d, i) => {
       if (this.pathFill) {
         const fill = this.pathFill[i] ?? 'currentColor'
