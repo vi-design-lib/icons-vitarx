de367779b65de7b4c6e96032ba722e9f79d63d9f
ZhuChongLin <8210856@qq.com>
2025-03-01 22:55:24 +0800

    docs(Icons): 更新自定义图标使用说明
    - 明确指出使用自定义图标需要在 index.html 中导入相关资源
    - 提供了导入内置图标库和自定义图标库的示例代码
    - 补充了使用 'use'属性引用图标的说明
    - 不建议一次性导入所有内置图标，推荐按需引入以优化打包体积

--- src/widgets/Icons.ts
+++ src/widgets/Icons.ts
@@ -61,7 +61,19 @@ export interface IconProps {
   /**
    * 引用自定义图标
    *
-   * 使用该属性的前提必须在index.html中引入iconfont.js文件
+   * 要使用自定义图标，必须在`index.html`中导入相关的资源，
+   *
+   * ```html
+   * // 一次性导入内置图标库 550+ 图标
+   * // 并不建议这样做，使用组件方式使用图表，可以在打包时进行摇树优化，减小包体积
+   * <script src="@vi-design/icons-vitarx/iconfont.js"></script>
+   *
+   * // 导入自定义的图标库
+   * <script src="https://at.alicdn.com/t/c/font_4802092_0jhy18kzqt7.js"></script>
+   *
+   * 使用 use 属性引用图标
+   * <Icons use="icon-name"></Icons>
+   * ```
    *
    * @default ''
    */
